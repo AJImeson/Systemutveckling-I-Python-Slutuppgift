7507eaaa6ca2f9d8a811b0d01c9f0b8989357847
AJImeson <axel.imeson@chasacademy.se>
2025-09-30 11:59:30 +0200

    Update4 30/9

--- Monitoring/Monitoring_Class_Functions.py
+++ Monitoring/Monitoring_Class_Functions.py
@@ -4,17 +4,17 @@ import time
 
 class Monitor:
     
-    def Initialise_Monitoring(self): # Starts monitoring by user 
+    def Initialise_Monitoring(): # Starts monitoring by user 
     
         while True:
         
-            cpu_usage = psutil.cpu_percent(interval=5) # CPU Check
+            cpu_usage = psutil.cpu_percent(interval=2) # CPU Check
             ram_usage = psutil.virtual_memory().percent # RAM Check
             disk_usage = psutil.disk_usage('/').percent # Disk Usage 
     
             print(f"CPU Usage: {cpu_usage}% | RAM Usage: {ram_usage}% | Disk Usage: {disk_usage}%")
     
-            time.sleep(5) # Interval check for total monitor 
+            time.sleep(2) # Interval check for total monitor 
             
     
     def Monitoring_Mode(): # Automatic monitoring mode initialisation 
