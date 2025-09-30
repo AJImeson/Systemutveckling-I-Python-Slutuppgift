e41e847525fcc1eda609839bc462e7c50b0738ba
AJImeson <axel.imeson@chasacademy.se>
2025-09-30 09:14:32 +0200

    Update 30/9

--- Main/Main_User.py
+++ Main/Main_User.py
@@ -1,40 +1,70 @@
-from Monitoring import Monitoring_Mode, Initialise_Monitoring
-
+# from Monitoring import"
+from Menu_Functions import clear_screen
 
 print("---------------------------------")
 print("Program menu | Select and option")
 print("---------------------------------\n")
 
-main_system_menu = input("[1] Commence Monitoring\n[2] Show Current Monitoring Activity\n[3] Configure Alerts\n[4] Alert List\n[5] Commence Monitoring Mode\n")
+main_system_menu = input("[1] Start Monitoring\n[2] Show Current Monitoring Activity\n[3] Configure Alerts\n[4] Alert List\n[5] Commence Monitoring Mode\n[6] Quit Program\n")
 
 while True: 
+    clear_screen()
+    try:
     
-    match main_system_menu:
-        
-        case "1":
+        match main_system_menu:
             
-            Initialise_Monitoring()
-        
-        case "2":
-            pass
-        
-        case "3":
+            case "1":
+                clear_screen()
+                pass
             
-            while True:
+            case "2":
+                clear_screen()
+                pass 
+            
+            case "3":
+                clear_screen()
                 
                 print("----------------------------")
                 print("Choose an Alert to configure")
                 print("----------------------------\n")
-                
+                    
                 configure_menu = input("[1] CPU Usage\n[2] Memory Usage\n[3] Disk Usage\n[4] Exit to Main Menu\n")
                 
+                while True:
+                    clear_screen()
+                    
+                    match configure_menu:
+                            
+                    
+                        case "1":
+                            pass
+                                
+                        case "2":
+                            pass
                 
-        
-        case "4":
+                        case "3":
+                            pass
+                         
             
-                Monitoring_Mode()
-        
-        case "5":
-            pass 
-    
-    
+            case "4":
+                clear_screen()
+                pass 
+                    
+            
+            case "5":
+                clear_screen()
+                pass
+            
+            case "6":
+                print("Terminating Program....")
+                print("Thank you for using")
+                break
+            
+            case _:
+                
+                print("Invalid input - Choose between 1-6")
+                continue
+                
+    except ValueError(Exception):
+        print("Unknown error occured")
+        continue 
